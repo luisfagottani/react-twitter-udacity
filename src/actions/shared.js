52fcbd97bf7be4b75fa3df26c9d48fb8d39efc5c
Luis Felipe <asp.r2.agottani@gmail.com>
2019-02-07 16:07:40 -0200

    Creating all actions

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -1,12 +1,16 @@
 import { getInitialData } from "../utils/api/";
 import { receiveTweets } from "./tweets";
 import { receiveUsers } from "./users";
+import { setAuthUser } from "./authedUser";
+
+const USER_ID = "dan_abramov";
 
 export function handleInitialData() {
   return dispatch => {
     return getInitialData().then(({ users, tweets }) => {
       dispatch(receiveTweets(tweets));
       dispatch(receiveUsers(users));
+      dispatch(setAuthUser(USER_ID));
     });
   };
 }
